1abdbcc7079e7c2e4b927cbf2f33ebb6ba155b12
notnotharsh <unagctystudent2@gmail.com>
2019-05-19 22:15:09 -0400

    Original Test

---
+++ app/src/main/java/org/team1257/lightalarm/WhiteScreen.java
@@ -0,0 +1,19 @@
+package org.team1257.lightalarm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhiteScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.white_screen);
+    }
+    public void otherActivity(android.view.View view) {
+        Intent startIntent = new Intent();
+        startIntent.setClassName(this.getApplicationContext(), MainActivity.class.getName());
+        startIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        this.getApplicationContext().startActivity(startIntent);
+    }
+}
