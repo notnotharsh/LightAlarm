540ec1e4a40d0c9d9261b0f00d04f0cd30928c15
notnotharsh <unagctystudent2@gmail.com>
2019-05-27 10:42:48 -0400

    First test

--- app/src/main/java/org/oconnorproject/lightalarm/WhiteScreen.java
+++ app/src/main/java/org/oconnorproject/lightalarm/WhiteScreen.java
@@ -1,14 +1,23 @@
 package org.oconnorproject.lightalarm;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.provider.Settings.System;
 
 public class WhiteScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.white_screen);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON, WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        System.putInt(getContentResolver(), System.SCREEN_BRIGHTNESS, 255);
+        WindowManager.LayoutParams layoutPars = getWindow().getAttributes();
+        layoutPars.screenBrightness = 1;
+        getWindow().setAttributes(layoutPars);
     }
     public void otherActivity(android.view.View view) {
         Intent startIntent = new Intent();
